691d8d85e2d43b8a6d37c569af9f53de249e4c7f
Sirisha Ayyagari <ayyagari.siri@gmail.com>
2020-05-07 22:46:15 +0200

    Random Numbers

--- src/main/java/be/intecbrussel/servlets/RandomNumberServlets.java
+++ src/main/java/be/intecbrussel/servlets/RandomNumberServlets.java
@@ -7,24 +7,33 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Random;
 
-@WebServlet("/Hello")
-public class HelloWorldServlets extends HttpServlet {
+@WebServlet("/Random")
+public class RandomNumberServlets extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
+
         PrintWriter writer = resp.getWriter();
         writer.println("<html lang=\"en\">\n" +
                 "<head>\n" +
                 "    <meta charset=\"UTF-8\">\n" +
-                "    <title>sirisha page</title>\n" +
+                "    <title>random number page</title>\n" +
                 "</head>\n" +
                 "<body>\n" +
-                "<h1>Sirisha</h1>\n" +
+                "<h1>" + "random number = " +
+                getRandomNumber() + "</h1>\n" +
                 "</body>\n" +
                 "</html>"
         );
         writer.close();
+    }
+
+    private int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(100);
     }
 }
